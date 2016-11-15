98c7c3d3a3a25889405cfe17e0c1f7ac03b3f556
Evangelos Tzemis <Etzemis@Evangeloss-MacBook-Pro-2.local>
2016-11-15 19:49:12 +0200

    Fixed API for Registration, Login, User Activation, User Deactivation, Update Location of User, Fetch Travellers Around User, insertDestination of user, Trip Fetch Travellers Around User (handles proximity)

--- app/model/User.js
+++ app/model/User.js
@@ -4,12 +4,26 @@ var Schema = mongoose.Schema;
 //var Coordinate = require('./coordinate');
 var SchemaTypes = mongoose.Schema.Types;
 
+
+//*********************************
+//*** Status Of The User ***
+//
+//  statusMode O: InActive
+//  statusMode 1: Active - Thus Invisible
+//  statusMode 2: Has Destination - Visible
+//  statusMode 3: In Trip Request
+//  statusMode 4: In Trip
+//
+//*********************************
+
+
 var coordinateSchema = new Schema({
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true }
 });
 // create a schema
 var userSchema = new Schema({
+  statusMode: {type: Number, required: true},
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -29,7 +43,8 @@ var userSchema = new Schema({
     {
       type: [Number],
       index: '2d'
-    }
+    },
+    extraPersons: {type: Number}
   }
 });
 
--- index.js
+++ index.js
@@ -68,7 +68,6 @@ var auth = function (req, res, next)
 		}
 		if (userLocal)
 		{
-			console.log('Basic Auth: Authorized');
 			if (passwordHash.verify(user.name + userLocal.password, user.pass))
 			{   // pass is the hashed string
 				next();
@@ -142,6 +141,7 @@ app.post('/register', function (req, res) {
 			{
 				//Create User
 				var newUser = new User({
+					statusMode: 0,
 					username: req.body.username,
 					email: req.body.email,
 					password: req.body.password,
@@ -160,6 +160,7 @@ app.post('/register', function (req, res) {
 					}
 					else
 					{
+						console.log("\nRegistration: User "+req.body.email+"\n")
 						res.json({success: "User Registration Successful"});
 					}
 				});
@@ -171,106 +172,287 @@ app.post('/register', function (req, res) {
 
 
 //**************************************
-//
+// Login
 //**************************************
-// Login User
-app.post('/login', function (req, res) {
-	console.log(req.body);      // your JSON
+app.post('/login', function (req, res)
+{
 
-	User.findOne({ 'email': req.body.email }, 'username imageUrl password secretToken', function (err, user) {
-  		if (err) {
+	User.findOne({ 'email': req.body.email }, 'username imageUrl password secretToken', function (err, user)
+	{
+  		if (err)
+  		{
   			console.log(err)
 			res.status(500).send('Server Internal Error')
   		}
-  		else {
-            if (!user){
+  		else
+		{
+            if (!user)
+            {
                 res.json({message: "Invalid Username or Password"})
             }
             else {
-                console.log('LOGIN: found.user')
 
                 res.setHeader('Content-Type', 'application/json');
-
-                if (req.body.password != user.password) {
+                if (req.body.password != user.password)
+                {
                     res.json({message: "Invalid Username or Password"})
-                } else {
-                    var jsonToReturn = {token: user.secretToken,
-                        username: user.username,
-                        imageUrl: user.imageUrl};
-                    console.log(jsonToReturn)
-                    res.json(jsonToReturn);
+                }
+                else
+				{
+					User.update({'email': req.body.email}, {$set: {'statusMode': 1}}, function (err, result)
+					{
+						console.log(result)
+						if (result)
+						{
+							var jsonToReturn = {
+								token: user.secretToken,
+								username: user.username,
+								imageUrl: user.imageUrl};
 
-                    console.log("LOGIN: Valid User")
+							res.json(jsonToReturn);
+
+							console.log("\nLog-In: User "+req.body.email+"\n")
+						}
+						else
+						{
+							console.log(err)
+							res.status(500).send('Server Internal Error')
+						}
+					});
+
+
+
                 }
             }
         }
 	})
 });
 
-
 //**************************************
-//
+// Activate User
 //**************************************
-// Update the location of a specific user
-app.post('/location', auth, function (req, res) {
-    // If internal error in Saving or whatever
-    // res.status(500).send('Server Internal Error')
-    // If everything is successful
-    // res.send("<html><body><h1>Post Location was Successful</h1></body></html>");
-	console.log(req.body);
-    res.send("<html><body><h1>Post Location was Successful</h1></body></html>");
+app.get('/user/activate', auth, function (req, res)
+{
+	var user = basicAuth(req);
 
+	User.update({'email': user.name}, {$set: {'statusMode': 1}}, function (err, result)
+	{
+		console.log(result)
+		if (result)
+		{
+			res.send("<html><body><h1> Deactivated User</h1></body></html>");
+
+			console.log("\nActivated: User "+ user.name+"\n")
+		}
+		else
+		{
+			console.log(err)
+			res.status(500).send('Server Internal Error')
+		}
+	});
 });
 
+//**************************************
+// Deactivate User
+//**************************************
+app.get('/user/deactivate', function (req, res)
+{
+	var user = basicAuth(req);
+
+	User.update({'email': user.name}, {$set: {'statusMode': 0}}, function (err, result)
+	{
+		console.log(result)
+		if (result)
+		{
+			res.send("<html><body><h1> Deactivated User</h1></body></html>");
+
+			console.log("\nDeactivated: User "+ user.name+"\n")
+		}
+		else
+		{
+			console.log(err)
+			res.status(500).send('Server Internal Error')
+		}
+	});
+});
+
+
+//**************************************
+// Update Location of User
+//**************************************
+
+app.post('/location', auth, function (req, res)
+{
+	var user = basicAuth(req);
+	User.update({'email': user.name}, {$set: {'location': [(req.body.longitude), (req.body.latitude)]}}, function (err, result)
+	{
+		if (err)
+		{
+			console.log(err);
+			res.status(500).send('Server Internal Error');
+		}
+		if (result)
+		{
+			console.log('Location Update: '+user.name);
+			res.send("<html><body><h1>Post Location was Successful</h1></body></html>");
+		}
+		else
+		{
+			res.send(401).send('User Not Found');
+		}
+	});
+});
+
+
+//**************************************
+// Travellers Around ME
+//**************************************
 
 // Returns the travellers that are around the user
-app.get('/travellers', auth, function (req, res) {
-    console.log(req.body)
-    var jsonToReturn = [{
-        username: "Thanasis",
-        address: "Ikoniou 4",
-        region: "Nikaia",
-        currentLatitude: 37.983709,
-        currentLongitude: 23.680877,
-        destinationLatitude: 37.997272,
-        destinationLongitude:23.686664,
-        extraPersons: 2,
-		imageUrl: "photo1.jpg"
-    },
-    {
-        username: "Alexis",
-        address: "Argirokastrou",
-        region: "Aigaleo",
-        currentLatitude: 37.984470,
-        currentLongitude: 23.680367,
-        destinationLatitude: 37.997272,
-        destinationLongitude:23.686664,
-        extraPersons: 2,
-		imageUrl: "photo2.jpg"
-    }];
-    res.json(jsonToReturn)
+app.get('/travellers', auth, function (req, res)
+{
+	console.log("\nFetch Travellers\n")
+	var connectedUser = basicAuth(req);
+	User.findOne({'email': connectedUser.name}, 'location', function (err, user)
+	{
+		if (err)
+		{
+			res.status(500).send('Server Internal Error');
+		}
+		else
+		{
+			// console.log(user);
+			// limit results
+			var limit = 10;
+			// Filter Users with distance less than 500m
+			var maxDistance = 0.5;
 
-    // If username exists then return that
-    // res.json({message: "Username already Exists"});
-    // If internal error in Saving or whatever
-    // res.status(500).send('Server Internal Error')
+			// Convert the distance to radians
+			// Radius of Earth is approximately 6371 kilometers
+			maxDistance /= 6371;
+
+			// find a location
+			User.find({location: { $nearSphere: [user.location[0], user.location[1]], $maxDistance: maxDistance}, email: {$ne: connectedUser.name}, statusMode: {$eq: 2}}, "username email location destination imageUrl").limit(limit).exec(function(err, locations)
+			{
+				if (err)
+				{
+					res.status(500).send('Server Internal Error');
+				}
+				res.json(locations);
+			});
+		}
+	});
 });
 
 
 
-// Update the location of a specific user
-app.post('/destination', auth, function (req, res) {
-	console.log(req.body);
-	// res.send("<html><body><h1>Post Destination</h1></body></html>");
-	res.json({"OK": "Insert of Destination was successful"});
+//**************************************
+// Destination
+//**************************************
 
-    // If username exists then return that
-    // res.json({message: "Username already Exists"});
-    // If internal error in Saving or whatever
-    //res.status(401).send('Server Internal Error')
+app.post('/destination', auth, function (req, res)
+{
+
+	var user = basicAuth(req);
+	User.update({'email': user.name}, {$set: {	'destination.address': req.body.address,
+												'destination.region': req.body.region,
+												'destination.coordinates': [req.body.longitude,req.body.latitude],
+												'destination.extraPersons': req.body.extraPersons},
+												'statusMode': 2}, function (err, recordsUpdated)
+	{
+		if (err)
+		{
+			console.log(err);
+			res.status(500).send('Server Internal Error');
+		}
+		if (recordsUpdated)
+		{
+			console.log('User location updated successfully!');
+			User.findOne({'email': user.name}, function (err, user)
+			{
+				console.log(user);
+			});
+			res.send("<html><body><h1>Post destination was successful</h1></body></html>");
+		}
+		else
+		{
+			console.log('User not found !');
+		}
+		res.send("<html><body><h1>Post destination was unuccessful :( </h1></body></html>");
+		res.json({"OK": "Insert of Destination was successful"});
+
+	});
 });
 
 
+
+//**************************************
+// TRIP Travellers Around Me
+//**************************************
+
+// Returns the travellers that are around the user
+app.get('/trip/travellers', auth, function (req, res)
+{
+	console.log("\nFetch Travellers\n")
+	var connectedUser = basicAuth(req);
+	User.findOne({'email': connectedUser.name}, 'destination', function (err, user)
+	{
+		if (err)
+		{
+			res.status(500).send('Server Internal Error');
+		}
+		else
+		{
+			// console.log(user);
+			// limit results
+			var limit = 10;
+
+
+			// Convert the distance to radians
+			// Radius of Earth is approximately 6371 kilometers
+			earthRadiusConstant = 6371;
+			// Filter Users with distance less than 500m
+			var highProximityDistance = 1/earthRadiusConstant;
+			var mediumProximityDistance = 2/earthRadiusConstant;
+			var lowProximityDistance = 5/earthRadiusConstant;
+
+
+
+			// find a location
+			User.find({'destination.coordinates': {$nearSphere: [user.destination.coordinates[0], user.destination.coordinates[1]], $maxDistance: highProximityDistance}, 'email': {$ne: connectedUser.name} }, "username email location destination imageUrl").limit(limit).sort().exec(function(err, highProxDestinations)
+			{
+				if (err)
+				{
+					res.status(500).send('Server Internal Error');
+				}
+				User.find({'destination.coordinates': {$nearSphere: [user.destination.coordinates[0], user.destination.coordinates[1]], $minDistance: highProximityDistance, $maxDistance: mediumProximityDistance}, 'email': {$ne: connectedUser.name} }, "username email location destination imageUrl").limit(limit).sort().exec(function(err, medProxDestinations)
+				{
+					if (err)
+					{
+						res.status(500).send('Server Internal Error');
+					}
+					User.find({'destination.coordinates': {$nearSphere: [user.destination.coordinates[0], user.destination.coordinates[1]], $minDistance: mediumProximityDistance, $maxDistance: lowProximityDistance}, 'email': {$ne: connectedUser.name} }, "username email location destination imageUrl").limit(limit).sort().exec(function(err, lowProxDestinations)
+					{
+						if (err)
+						{
+							res.status(500).send('Server Internal Error');
+						}
+						var jsonToReturn = {
+							"0": highProxDestinations,
+							"1": medProxDestinations,
+							"2":lowProxDestinations
+						}
+						res.json(jsonToReturn);
+					});
+				});
+			});
+
+
+
+
+		}
+	});
+});
 
 
 
